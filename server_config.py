e7bda795b8255bf596c33980738f90479656bb2c
MJ-meo-dmt <francois.debruyn66@gmail.com>
2012-01-19 08:39:12 +0200

    Server side configs, db stuff atm.

---
+++ server_config.py
@@ -0,0 +1,18 @@
+# Config file for server related things.
+
+## [General] ##
+
+
+
+
+
+
+## [Database] ##
+
+# We can have everything in one big database?
+# Well anyway we will talk about it.
+# spell_db will be on client side as well.
+# item_db aslo. 
+SERVER_DB = "database/server_db" # All user related shit.
+SPELL_DB = "database/spell_db" # All spell data.
+ITEM_DB = "database/item_db" # All items if we have any?
